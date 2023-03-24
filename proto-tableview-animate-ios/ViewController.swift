b4fc7e6cab1c2dc621a4f8522f7521f9d312dc2d
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-24 12:41:57 -0500

    Initial Commit

---
+++ proto-tableview-animate-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-tableview-animate-ios
+//
+//  Created by Santosh Krishnamurthy on 3/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
